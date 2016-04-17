96b3e4617067778a990ae1b7eea6c2e36b1ba1e9
FellenMeister <leslie_fellensiek@hotmail.com>
2016-04-17 21:45:08 +0200

    Shit is verwijderd

--- ihff/Controllers/HomeController.cs
+++ ihff/Controllers/HomeController.cs
@@ -10,22 +10,21 @@ namespace ihff.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Disclaimer()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult PrivacyPolicy()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult SiteMap()
+        {
             return View();
         }
     }
