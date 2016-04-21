1ea1877f2b5b1d5a837c835121c13eec67dec0bc
Remflex <remco-guitar@live.nl>
2016-04-21 22:00:49 +0200

    Film en special class
    properties er in gezet + lege constructor

--- ihff/Models/Film/Film.cs
+++ ihff/Models/Film/Film.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ihff.Models
 {
     public class Film
     {
-    }
+        // Properties
+        [Required][Key]
+        public int Film_Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Short_Description { get; set; }
+        public string Full_Description { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public int Year { get; set; }
+        public float Rating { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public int Runtime { get; set; }
+        public string Age { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Cast { get; set; }
+        public string Hyperlink { get; set; }
+
+        // Constructor
+        public Film() { }
+        
+        }
 }
--- ihff/Models/Special/Special.cs
+++ ihff/Models/Special/Special.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ihff.Models
 {
     public class Special
     {
+        // Properties
+        [Required][Key]
+        public int Special_Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Short_Description { get; set; }
+        public string Full_Description { get; set; }
+        [Required]
+        public string Led_By { get; set; }
+        public string Extra { get; set; }
+
+        // Constructor
+        public Special() { }
     }
 }
