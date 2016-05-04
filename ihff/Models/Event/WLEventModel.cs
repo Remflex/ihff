48274d987c5d403bc175fbda917ed4c4c5cda19a
Mdejonge <mennodejonge94@gmail.com>
2016-05-04 17:32:01 +0200

    Gekloot met shit naar wishlist toe sturen

---
+++ ihff/Models/Event/WLEventModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ihff.Models
+{
+    public class WLEventModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Soort { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+
+        public WLEventModel(int Id, string Name, string Soort, string Day, string Time, int Quantity, float Price)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Soort = Soort;
+            this.Day = Day;
+            this.Time = Time;
+            this.Quantity = Quantity;
+            this.Price = Price;
+        }
+    }
+}
---
+++ ihff/Models/EventWishList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ihff.Models
+{
+    public class EventWishList
+    {
+        public List<WLEventModel> Events{ get; set; }
+
+        public EventWishList(List<WLEventModel> events)
+        {
+            this.Events = events;
+        }
+    }
+}
